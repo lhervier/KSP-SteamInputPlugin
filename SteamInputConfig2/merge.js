16511b4f92f9cae953be73431abbc571422d4407
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2025-06-27 20:09:26 +0200

    Sauve le controller vdf final

--- SteamInputConfig2/merge.js
+++ SteamInputConfig2/merge.js
@@ -3,8 +3,23 @@ const path = require('path');
 const VDF = require('vdf-parser');
 const { saveVdfFile, loadVdfFile } = require('./vdf-utils');
 
+// Load the root controller file, resolving #ref
 const vdf = loadVdfFile(
     '.', 
     'controller_steamcontroller_gordon.vdf'
 );
-console.log(vdf);
+
+// Create the "build" directory if it doesn't exist
+const buildDir = path.join(__dirname, 'build');
+if (!fs.existsSync(buildDir)) {
+    fs.mkdirSync(buildDir, { recursive: true });
+}
+
+// Save the merged VDF file
+saveVdfFile(
+    vdf, 
+    path.join(buildDir, 'controller_steamcontroller_gordon.vdf')
+);
+
+console.log('VDF file merged and saved successfully.');
+
