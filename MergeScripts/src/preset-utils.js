8dd2aadbaa2133a7dc18b3743aac9313d3d36462
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2026-05-13 19:41:43 +0200

    Un preset ne peut pas binder deux fois sur le même groupe

--- MergeScripts/src/preset-utils.js
+++ MergeScripts/src/preset-utils.js
@@ -2,6 +2,9 @@ function resolvePresets(vdf, groupIds) {
     for( const preset of vdf.controller_mappings.preset ) {
         const result = {};
         for( const [key, value] of Object.entries(preset.group_source_bindings) ) {
+            if( Array.isArray(value) ) {
+                throw new Error(`Preset "${preset.name}": group "${key}" is referenced multiple times in group_source_bindings. Each group must be unique within a preset.`);
+            }
             if( groupIds[key] === undefined ) {
                 throw new Error(`Unable to resolve preset id for ${key}`);
             }
