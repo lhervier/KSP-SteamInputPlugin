3d800f50602e7374bf858c5d9f03c589dad8eb45
Lionel HERVIER <lhervier@asi.fr>
2026-05-19 18:05:02 +0200

    Mise à jour du script de build sous windows

--- build-plugin.bat
+++ build-plugin.bat
@@ -7,7 +7,7 @@ echo Detection de la structure KSP
 echo -------------------------------------------
 
 REM Vérifier si KSPDIR est défini
-if "%KSPDIR%"=="" (
+if not defined KSPDIR (
     echo ERREUR: La variable d'environnement KSPDIR n'est pas définie
     echo Veuillez définir KSPDIR avec le chemin vers votre installation KSP
     echo Exemple: set "KSPDIR=C:\Program Files ^(x86^)\Steam\steamapps\common\Kerbal Space Program"
@@ -15,20 +15,21 @@ if "%KSPDIR%"=="" (
 )
 
 REM Vérifier que les DLLs KSP existent (Windows ou Linux)
-if exist "%KSPDIR%\KSP_x64_Data\Managed\Assembly-CSharp.dll" (
+REM Utiliser !KSPDIR! dans les blocs ( ) : les parenthèses de "(x86)" cassent sinon le parsing batch
+if exist "!KSPDIR!\KSP_x64_Data\Managed\Assembly-CSharp.dll" (
     echo Structure Windows détectée ^(KSP_x64_Data^)
-    set "KSP_DATA_DIR=%KSPDIR%\KSP_x64_Data"
-) else if exist "%KSPDIR%\KSP_Data\Managed\Assembly-CSharp.dll" (
+    set "KSP_DATA_DIR=!KSPDIR!\KSP_x64_Data"
+) else if exist "!KSPDIR!\KSP_Data\Managed\Assembly-CSharp.dll" (
     echo Structure Linux détectée ^(KSP_Data^)
-    set "KSP_DATA_DIR=%KSPDIR%\KSP_Data"
+    set "KSP_DATA_DIR=!KSPDIR!\KSP_Data"
 ) else (
-    echo ERREUR: Assembly-CSharp.dll non trouvé dans %KSPDIR%\KSP_x64_Data\Managed\ ou %KSPDIR%\KSP_Data\Managed\
+    echo ERREUR: Assembly-CSharp.dll non trouvé dans !KSPDIR!\KSP_x64_Data\Managed\ ou !KSPDIR!\KSP_Data\Managed\
     echo Vérifiez que KSPDIR pointe vers le bon répertoire KSP
     exit /b 1
 )
 
-echo Utilisation de KSPDIR: %KSPDIR%
-echo Utilisation de KSP_DATA_DIR: %KSP_DATA_DIR%
+echo Utilisation de KSPDIR: !KSPDIR!
+echo Utilisation de KSP_DATA_DIR: !KSP_DATA_DIR!
 
 echo.
 echo -------------------------------------------
