b9fb10d7b9028eb07979efec20eae0c9d1611df1
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2026-05-22 10:29:11 +0200

    oups

--- SteamInputPlugin/Src/ui/ugui/TitleBarBuilder.cs
+++ SteamInputPlugin/Src/ui/ugui/TitleBarBuilder.cs
@@ -7,7 +7,62 @@ namespace com.github.lhervier.ksp.ui.ugui
 {
     public class TitleBarBuilder
     {
+        
         public static GameObject Create(string objectName)
+        {
+            var titleBarGo = new GameObject(objectName, typeof(RectTransform));
+
+            // If the parent has a layout (and that's the case), forget about me, I will position elements myself.
+            var titleBarLayout = titleBarGo.AddComponent<LayoutElement>();
+            titleBarLayout.ignoreLayout = true;
+
+            // Title bar zone relative to the parent, stretched horizontaly
+            var titleBarRect = titleBarGo.GetComponent<RectTransform>();
+            titleBarRect.anchorMin = new Vector2(0f, 1f);
+            titleBarRect.anchorMax = new Vector2(1f, 1f);
+            titleBarRect.pivot = new Vector2(0.5f, 1f);
+            titleBarRect.sizeDelta = new Vector2(0f, SteamInputPalette.TitleBarHeight);
+            titleBarRect.anchoredPosition = Vector2.zero;
+
+            // Image for the backgroup of the title bar
+            var headerImage = titleBarGo.AddComponent<Image>();
+            headerImage.sprite = SpritesGlobal.FillSprite;
+            headerImage.type = Image.Type.Simple;
+            headerImage.color = SteamInputPalette.Header;
+            headerImage.raycastTarget = false;
+
+            GameObject separatorGo = CreateSeparator("BottomBorder");
+            separatorGo.transform.SetParent(titleBarGo.transform, false);
+
+            GameObject rootGo = CreateRoot("Root");
+            rootGo.transform.SetParent(titleBarGo.transform, false);
+
+            return titleBarGo;
+        }
+
+        public static GameObject CreateSeparator(string objectName)
+        {
+            var separatorGo = new GameObject(objectName, typeof(RectTransform));
+            
+            // Stretched horizontally, positionned at the bottom of the parent
+            var separatorRect = separatorGo.GetComponent<RectTransform>();
+            separatorRect.anchorMin = new Vector2(0f, 0f);
+            separatorRect.anchorMax = new Vector2(1f, 0f);
+            separatorRect.pivot = new Vector2(0.5f, 0f);
+            separatorRect.sizeDelta = new Vector2(0f, SteamInputPalette.TitleBarSeparatorHeight);
+            separatorRect.anchoredPosition = Vector2.zero;
+            
+            // The separator
+            var separatorImage = separatorGo.AddComponent<Image>();
+            separatorImage.sprite = SpritesGlobal.FillSprite;
+            separatorImage.type = Image.Type.Simple;
+            separatorImage.color = SteamInputPalette.TitleBarSeparatorColor;
+            separatorImage.raycastTarget = false;
+
+            return separatorGo;
+        }
+
+        public static GameObject CreateRoot(string objectName)
         {
             var rowGo = CreateLeftRow("LeftRow");
 
