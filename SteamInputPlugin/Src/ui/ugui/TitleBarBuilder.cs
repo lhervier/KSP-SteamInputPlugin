67d0e9383dcbfe61fdfbe47a3b44d7ede80e8661
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2026-05-22 09:18:16 +0200

    Reorganisation du code

--- SteamInputPlugin/Src/ui/ugui/TitleBarBuilder.cs
+++ SteamInputPlugin/Src/ui/ugui/TitleBarBuilder.cs
@@ -9,53 +9,20 @@ namespace com.github.lhervier.ksp.ui.ugui
     {
         public static GameObject Create(string objectName)
         {
-            // Main title bar object
-            // -----------------------------------
-            var titleBarGo = new GameObject(objectName, typeof(RectTransform));
+            var rowGo = CreateLeftRow("LeftRow");
 
-            // If the parent has a layout (and that's the case), forget about me, I will position elements myself.
-            var titleBarLayout = titleBarGo.AddComponent<LayoutElement>();
-            titleBarLayout.ignoreLayout = true;
+            var iconGo = CreateIcon("GamepadIcon");
+            iconGo.transform.SetParent(rowGo.transform, false);
 
-            // Title bar zone relative to the parent, stretched horizontaly
-            var titleBarRect = titleBarGo.GetComponent<RectTransform>();
-            titleBarRect.anchorMin = new Vector2(0f, 1f);
-            titleBarRect.anchorMax = new Vector2(1f, 1f);
-            titleBarRect.pivot = new Vector2(0.5f, 1f);
-            titleBarRect.sizeDelta = new Vector2(0f, SteamInputPalette.TitleBarHeight);
-            titleBarRect.anchoredPosition = Vector2.zero;
-
-            // Image for the backgroup of the title bar
-            var headerImage = titleBarGo.AddComponent<Image>();
-            headerImage.sprite = SpritesGlobal.FillSprite;
-            headerImage.type = Image.Type.Simple;
-            headerImage.color = SteamInputPalette.Header;
-            headerImage.raycastTarget = false;
-
-            // Game object for the bottom border
-            // ------------------------------------
-            var bottomBorderGo = new GameObject("BottomBorder", typeof(RectTransform));
-            bottomBorderGo.transform.SetParent(titleBarGo.transform, false);
-
-            // Stretched horizontally, positionned at the bottom of the parent
-            var bottomBorderRect = bottomBorderGo.GetComponent<RectTransform>();
-            bottomBorderRect.anchorMin = new Vector2(0f, 0f);
-            bottomBorderRect.anchorMax = new Vector2(1f, 0f);
-            bottomBorderRect.pivot = new Vector2(0.5f, 0f);
-            bottomBorderRect.sizeDelta = new Vector2(0f, SteamInputPalette.TitleBarSeparatorHeight);
-            bottomBorderRect.anchoredPosition = Vector2.zero;
+            var labelGo = CreateLabel("TitleLabel");
+            labelGo.transform.SetParent(rowGo.transform, false);
             
-            // The separator
-            var bottomBorderImage = bottomBorderGo.AddComponent<Image>();
-            bottomBorderImage.sprite = SpritesGlobal.FillSprite;
-            bottomBorderImage.type = Image.Type.Simple;
-            bottomBorderImage.color = SteamInputPalette.TitleBarSeparatorColor;
-            bottomBorderImage.raycastTarget = false;
+            return rowGo;
+        }
 
-            // Game object for the main row
-            // -----------------------------------------
-            var rowGo = new GameObject("Row", typeof(RectTransform));
-            rowGo.transform.SetParent(titleBarGo.transform, false);
+        private static GameObject CreateLeftRow(string objectName)
+        {
+            var rowGo = new GameObject(objectName, typeof(RectTransform));
 
             // Full size of the parent = the title bar, minus the bottom separator
             var rowRect = rowGo.GetComponent<RectTransform>();
@@ -79,10 +46,12 @@ namespace com.github.lhervier.ksp.ui.ugui
             layout.childForceExpandWidth = false;
             layout.childForceExpandHeight = false;
 
-            // Icon Game Object
-            // ---------------------------
-            var iconGo = new GameObject("GamepadIcon", typeof(RectTransform));
-            iconGo.transform.SetParent(rowGo.transform, false);
+            return rowGo;
+        }
+
+        private static GameObject CreateIcon(string objectName)
+        {
+            var iconGo = new GameObject(objectName, typeof(RectTransform));
 
             // The icon itself
             var iconImage = iconGo.AddComponent<Image>();
@@ -101,13 +70,13 @@ namespace com.github.lhervier.ksp.ui.ugui
                     SteamInputPalette.DefaultIconSize
                 );
             }
+            return iconGo;
+        }
 
-            // Windows title game object
-            // -----------------------------
-            var labelGo = new GameObject("TitleLabel", typeof(RectTransform));
-            labelGo.transform.SetParent(rowGo.transform, false);
+        private static GameObject CreateLabel(string gameObject)
+        {
+            var labelGo = new GameObject(gameObject, typeof(RectTransform));
             
-            // The label component
             var label = labelGo.AddComponent<Text>();
             label.text = ModLocalization.GetString("SteamInput_titleHelp").ToUpperInvariant();
             label.font = HighLogic.UISkin.font;
@@ -119,7 +88,7 @@ namespace com.github.lhervier.ksp.ui.ugui
             label.verticalOverflow = VerticalWrapMode.Overflow;
             label.raycastTarget = false;
 
-            return titleBarGo;
+            return labelGo;
         }
     }
 }
