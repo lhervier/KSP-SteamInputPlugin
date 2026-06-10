0ba6dce2d14218e77693b54c4632478e00632da5
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2026-06-10 09:15:51 +0200

    Vector2 est non nullable

--- SteamInputPlugin/Src/ui/ugui/ModPopupBuilder.cs
+++ SteamInputPlugin/Src/ui/ugui/ModPopupBuilder.cs
@@ -14,23 +14,28 @@ namespace com.github.lhervier.ksp.steaminput.ui.ugui
     {
         private const string DIALOG_ID = "SteamInputCheatSheetUGUI";
         
-        private CheatSheetViewModel _viewModel;
-        
-        private Vector2 _position;
-        
         // =============================================
         // Build parameters
         // =============================================
 
+        private CheatSheetViewModel _viewModel;
         public ModPopupBuilder ViewModel(CheatSheetViewModel viewModel)
         {
             this._viewModel = viewModel;
             return this;
         }
 
+        private Vector2 _position;
+        private bool _hasPosition;
         public ModPopupBuilder Position(Vector2 position)
         {
             this._position = position;
+            this._hasPosition = true;
+            return this;
+        }
+        public ModPopupBuilder ResetPosition()
+        {
+            this._hasPosition = false;
             return this;
         }
 
@@ -44,7 +49,7 @@ namespace com.github.lhervier.ksp.steaminput.ui.ugui
         /// </summary>
         public ModPopupDialogController Build()
         {
-            var popupController = new PopupBuilder<TitleBarController, BodyController>()
+            var popupBuilder = new PopupBuilder<TitleBarController, BodyController>()
                 .PopupID(DIALOG_ID)
                 .Title(ModLocalization.GetString("SteamInput_titleHelp"))
                 .Icon(SpritesTitleBar.GamepadIconSprite)
@@ -54,9 +59,12 @@ namespace com.github.lhervier.ksp.steaminput.ui.ugui
                 .ContentBuilder(
                     new BodyBuilder().ViewModel(_viewModel)
                 )
-                .Size(new Vector2(SteamInputPalette.WindowWidth, SteamInputPalette.WindowHeight))
-                .Position(this._position)
-                .Build();
+                .Size(new Vector2(SteamInputPalette.WindowWidth, SteamInputPalette.WindowHeight));
+            if( this._hasPosition )
+            {
+                popupBuilder = popupBuilder.Position(this._position);
+            }
+            PopupController popupController = popupBuilder.Build();
             if( popupController == null ) return null;
 
             OverlayController overlayController = new OverlayBuilder()
